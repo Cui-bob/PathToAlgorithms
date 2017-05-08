942c424f1647c3fb199e2f806d4acc25dfa5c12b
Zhijie Cui <bob940720@outlook.com>
2017-05-08 16:10:26 -0400

    Fix incorrect sorting at index 0

--- src/Sorting/InsertionSort.java
+++ src/Sorting/InsertionSort.java
@@ -5,18 +5,22 @@ import java.util.Arrays;
 public class InsertionSort {
 	public static int[] insertionSort(int[] nums){
 		try{
-			int[] result = new int[nums.length];
+			int tmp;
 			for(int i=1;i<nums.length;i++){
-				for(int j=i-1;j>=0;j--){
-					if(result[j]>nums[i]){
-						result[j+1]=result[j];
-					}else{
-						result[j+1]=nums[i];
+				tmp = nums[i];
+				int j;
+				for(j=i-1;j>=0;j--){
+					if(nums[j]>tmp){
+						nums[j+1]=nums[j];
+					}
+					else{
 						break;
 					}
 				}
+				nums[j+1]=tmp;
 			}
-			return result;
+			
+			return nums;
 		}catch(Exception e){
 			e.printStackTrace();
 		}
